897697048a80d1b11ebac7792bb7f09534e40ea1
ProjectsAssesment <kumaramaduka5@gmail.com>
2024-04-16 16:16:57 +0530

    add test case

--- application/controllers/applicationController.test.js
+++ application/controllers/applicationController.test.js
@@ -2,6 +2,8 @@ describe("applicationController", () => {
   describe("postApplication", () => {
     test("validate user inputs", () => {});
 
+
+    
     test("Validate response", async () => {});
   });
 });
